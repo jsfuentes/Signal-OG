83da2da3aeb41940eb32f27b71fc68c2999731c3
Jorge Fuentes <jsjfuentesj@gmail.com>
2019-03-22 23:19:52 -0700

    Add async http

--- src/hackernews.py
+++ src/hackernews.py
@@ -1,40 +1,36 @@
-import requests
 import json 
-from dateutil.parser import parse
+import requests
 from datetime import datetime
-
-last_logged_in_utc = 1552268737
-# resp = requests.get(f"{BASE_URL}/maxitem.json")
-# curItem = resp.text
-# 
-# #hackernews can be deleted or null
-# while True:
-#     if resp.text == "null":
-#         item = json.loads(resp.text)
-#         print(item)
-#         if item['time'] <= last_logged_in_utc:
-#             break
-#         elif item['type'] == "story" and not item.get('deleted', False):
-#             print(item['title'], item['score'])
-#     curItem = str(int(curItem) - 1)
-#     # print(curItem)
+import aiohttp
+import asyncio
 
 class HackerNews:
     BASE_URL = "https://hacker-news.firebaseio.com/v0"
-    
-    def getItem(self, id):
-        resp = requests.get(f"{self.BASE_URL}/item/{id}.json")
-        return json.loads(resp.text)
-    
+
+    async def getItem(self, session, id):
+        async with session.get(f"{self.BASE_URL}/item/{id}.json") as resp:
+            rawItem = await resp.text()
+            return rawItem
+
+    async def getItems(self, ids):
+        items = []
+        async with aiohttp.ClientSession() as session:
+            for id in ids:
+                rawItem = await self.getItem(session, id)
+                item = json.loads(rawItem)
+                print(item)
+                items.append(item)
+        return items
+        
     def getTopStories(self):
         resp = requests.get(f"{self.BASE_URL}/topstories.json")
         ids = json.loads(resp.text)
+        ids = ids[:100]
         print(len(ids))
         dates = []
-        for id in ids: 
-            x = self.getItem(id)
-            dates.append(datetime.utcfromtimestamp(x['time']))
-        print(min(dates))
-        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.getItems(ids))
+# 
 hn = HackerNews()
 hn.getTopStories()
+
